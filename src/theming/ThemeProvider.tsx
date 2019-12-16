69b8dcfe660a79d5054943465c4ee890b58092a8
Sergey Alimov <sergey.alimov@gmail.com>
2019-12-16 17:11:38 +0400

    Rename appearance -> colorScheme

--- src/theming/ThemeProvider.tsx
+++ src/theming/ThemeProvider.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import deepmerge from 'deepmerge';
-import defaultTheme, { Appearance, ThemeFactory, resolveTheme } from './theme';
+import defaultTheme, { ColorSchemeName, ThemeFactory, resolveTheme } from './theme';
 import ThemeContext from './ThemeContext';
 
 interface Props {
-  appearance?: Appearance;
+  colorScheme?: ColorSchemeName;
   theme?: ThemeFactory;
   children?: React.ReactNode;
 }
 
-const ThemeProvider: React.FC<Props> = ({ appearance, theme, children }) => {
+const ThemeProvider: React.FC<Props> = ({ colorScheme, theme, children }) => {
   const value = React.useMemo(() => {
     return theme
-      ? deepmerge(resolveTheme(defaultTheme, appearance), resolveTheme(theme, appearance))
-      : resolveTheme(defaultTheme, appearance);
-  }, [appearance, theme]);
+      ? deepmerge(resolveTheme(defaultTheme, colorScheme), resolveTheme(theme, colorScheme))
+      : resolveTheme(defaultTheme, colorScheme);
+  }, [colorScheme, theme]);
 
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
 };
 
 ThemeProvider.defaultProps = {
-  appearance: 'light',
+  colorScheme: 'no-preference',
 };
 
 export default ThemeProvider;
--- src/theming/theme.ts
+++ src/theming/theme.ts
@@ -1,32 +1,32 @@
-export type Appearance = 'light' | 'dark';
+export type ColorSchemeName = 'light' | 'dark' | 'no-preference';
 
 export type Theme = {
   [key: string]: any;
 };
 
-export type ThemeFactory = Theme | ((appearance?: Appearance) => Theme);
+export type ThemeFactory = Theme | ((colorScheme?: ColorSchemeName) => Theme);
 
 // Colors based on Apple Human Interface Guidelines
 // https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/color/
-const defaultTheme: ThemeFactory = (appearance?: Appearance) => ({
+const defaultTheme: ThemeFactory = (colorScheme?: ColorSchemeName) => ({
   Colors: {
     white: '#ffffff',
     black: '#000000',
-    blue: appearance === 'dark' ? '#0a84ff' : '#007aff',
-    green: appearance === 'dark' ? '#30d158' : '#34c759',
-    indigo: appearance === 'dark' ? '#5e5ce6' : '#5856d6',
-    orange: appearance === 'dark' ? '#ff9f0a' : '#ff9500',
-    pink: appearance === 'dark' ? '#ff375f' : '#ff2d55',
-    purple: appearance === 'dark' ? '#bf5af2' : '#af52de',
-    red: appearance === 'dark' ? '#ff453a' : '#ff3b30',
-    teal: appearance === 'dark' ? '#64d2ff' : '#5ac8fa',
-    yellow: appearance === 'dark' ? '#ffd60a' : '#ffcc00',
+    blue: colorScheme === 'dark' ? '#0a84ff' : '#007aff',
+    green: colorScheme === 'dark' ? '#30d158' : '#34c759',
+    indigo: colorScheme === 'dark' ? '#5e5ce6' : '#5856d6',
+    orange: colorScheme === 'dark' ? '#ff9f0a' : '#ff9500',
+    pink: colorScheme === 'dark' ? '#ff375f' : '#ff2d55',
+    purple: colorScheme === 'dark' ? '#bf5af2' : '#af52de',
+    red: colorScheme === 'dark' ? '#ff453a' : '#ff3b30',
+    teal: colorScheme === 'dark' ? '#64d2ff' : '#5ac8fa',
+    yellow: colorScheme === 'dark' ? '#ffd60a' : '#ffcc00',
     gray: '#8e8e93',
-    gray2: appearance === 'dark' ? '#636366' : '#aeaeb2',
-    gray3: appearance === 'dark' ? '#48484a' : '#c7c7cc',
-    gray4: appearance === 'dark' ? '#3a3a3c' : '#d1d1d6',
-    gray5: appearance === 'dark' ? '#2c2c2e' : '#e5e5ea',
-    gray6: appearance === 'dark' ? '#1c1c1e' : '#f2f2f7',
+    gray2: colorScheme === 'dark' ? '#636366' : '#aeaeb2',
+    gray3: colorScheme === 'dark' ? '#48484a' : '#c7c7cc',
+    gray4: colorScheme === 'dark' ? '#3a3a3c' : '#d1d1d6',
+    gray5: colorScheme === 'dark' ? '#2c2c2e' : '#e5e5ea',
+    gray6: colorScheme === 'dark' ? '#1c1c1e' : '#f2f2f7',
   },
   Fonts: {
     thin: {
@@ -67,9 +67,12 @@ const defaultTheme: ThemeFactory = (appearance?: Appearance) => ({
   },
 });
 
-export function resolveTheme(theme: ThemeFactory = defaultTheme, appearance?: Appearance): Theme {
+export function resolveTheme(
+  theme: ThemeFactory = defaultTheme,
+  colorScheme?: ColorSchemeName,
+): Theme {
   if (theme instanceof Function) {
-    return theme(appearance);
+    return theme(colorScheme);
   }
 
   return theme;
