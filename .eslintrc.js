0efdd2a737e34a61159c0454c4ff9b55e38417bf
Nick Seryakov <nick.seryakov@gmail.com>
2019-12-16 15:29:09 +0400

    Adjust eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    // allow destructuring by ignoring the rest property's siblings
+    // requires to disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error", {
+      "ignoreRestSiblings": true
+    }]
+  }
 };
