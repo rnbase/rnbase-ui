3e7d1993e1c14ff584d2dac8195049cc6f1cf570
Nick Seryakov <nick.seryakov@gmail.com>
2019-12-28 04:01:40 +0400

    Update button tests

--- src/buttons/__tests__/Button.tsx
+++ src/buttons/__tests__/Button.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Animated as AnimatedObj, Text } from 'react-native';
 
 // Note: test renderer must be required after react-native.
-import TestRenderer from 'react-test-renderer';
+import TestRenderer, { act } from 'react-test-renderer';
 
 import { Props } from '../Button';
+
+jest.doMock('react-native/Libraries/Animated/src/Animated', () => {
+  const Animated = jest.requireActual('react-native/Libraries/Animated/src/Animated');
+
+  return {
+    ...Animated,
+    timing: jest.fn((value: any, { toValue }: any) => {
+      value.setValue(toValue);
+
+      return {
+        start: jest.fn(),
+      };
+    }),
+  };
+});
 
 const TouchableOpacity = (props: any) => React.createElement('TouchableOpacity', props);
 
@@ -15,31 +30,37 @@ jest.doMock('react-native/Libraries/Components/Touchable/TouchableOpacity', () =
  */
 const { default: Button } = jest.requireActual('../Button');
 
+jest.mock('react-native/Libraries/Animated/src/Easing', () => ({
+  in: (v: any) => `Easing.in(${v})`,
+  out: (v: any) => `Easing.out(${v})`,
+  exp: 'Easing.exp',
+}));
+
+const Animated: any = AnimatedObj;
+
 const createElement = (props: Props) => <Button {...props} />;
 
 const createRenderer = (props: Props) => TestRenderer.create(createElement(props));
-
-it('should render with custom styles', () => {
-  expect(createRenderer({ style: { backgroundColor: '#000000' } })).toMatchSnapshot();
-});
-
-it('should render as disabled', () => {
-  expect(createRenderer({ disabled: true })).toMatchSnapshot();
-});
 
 it('should render text only', () => {
   expect(createRenderer({ text: 'Button' })).toMatchSnapshot();
 });
 
-it('should render text with custom styles', () => {
-  expect(createRenderer({ text: 'Button', textStyle: { color: '#aaa' } })).toMatchSnapshot();
-});
-
-it('should render with image only', () => {
+it('should render image only', () => {
   expect(createRenderer({ imageSource: { uri: 'image.png' } })).toMatchSnapshot();
 });
 
-it('should render with left image', () => {
+it('should render as disabled', () => {
+  expect(
+    createRenderer({
+      text: 'Button',
+      imageSource: { uri: 'image.png' },
+      disabled: true,
+    }),
+  ).toMatchSnapshot();
+});
+
+it('should render image at the left', () => {
   expect(
     createRenderer({
       text: 'Right Text',
@@ -49,12 +70,24 @@ it('should render with left image', () => {
   ).toMatchSnapshot();
 });
 
-it('should render with right image', () => {
+it('should render image at the right', () => {
   expect(
     createRenderer({
       text: 'Left Text',
       imageSource: { uri: 'right.png' },
       imageAlignment: 'right',
+    }),
+  ).toMatchSnapshot();
+});
+
+it('should render with custom styles', () => {
+  expect(
+    createRenderer({
+      text: 'Button',
+      imageSource: { uri: 'image.png' },
+      style: { backgroundColor: 'red' },
+      textStyle: { color: 'green' },
+      imageStyle: { tintColor: 'blue' },
     }),
   ).toMatchSnapshot();
 });
@@ -75,4 +108,68 @@ it('should call onPress', () => {
   button.props.onPress();
 
   expect(onPress).toBeCalled();
+});
+
+describe('busy animation', () => {
+  const start = jest.fn();
+
+  Animated.timing.mockImplementation(() => ({ start }));
+
+  beforeEach(() => {
+    start.mockClear();
+  });
+
+  it('should not animate', () => {
+    const tree = createRenderer({});
+
+    act(() => {});
+
+    Animated.timing.mockClear();
+
+    act(() => {
+      tree.update(createElement({ busyIndicatorColor: '#ffffff' }));
+    });
+
+    expect(Animated.timing).not.toBeCalled();
+  });
+
+  it('should animate to busy', () => {
+    const tree = createRenderer({});
+
+    act(() => {});
+
+    Animated.timing.mockClear();
+
+    act(() => {
+      tree.update(createElement({ busy: true }));
+    });
+
+    expect(Animated.timing).toBeCalledWith(expect.objectContaining({ _value: 0 }), {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+      easing: 'Easing.out(Easing.exp)',
+    });
+    expect(start).toBeCalled();
+  });
+
+  it('should animate from busy', () => {
+    const tree = createRenderer({ busy: true });
+
+    act(() => {});
+
+    Animated.timing.mockClear();
+
+    act(() => {
+      tree.update(createElement({ busy: false }));
+    });
+
+    expect(Animated.timing).toBeCalledWith(expect.objectContaining({ _value: 1 }), {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+      easing: 'Easing.out(Easing.exp)',
+    });
+    expect(start).toBeCalled();
+  });
 });
