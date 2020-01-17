c930dbb2738b1e4839269442b624495b040aba2b
Nick Seryakov <nick.seryakov@gmail.com>
2020-01-17 04:51:38 +0400

    Adjust status bar color

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 
 import RootNavigator from './router';
 import theme from './theme';
 
 import { ThemeProvider } from '../..';
+
+StatusBar.setBarStyle('dark-content');
 
 const AppContainer = createAppContainer(RootNavigator);
 
