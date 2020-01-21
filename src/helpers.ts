b57fcf716c4d2d2dcd5399b02ae91a58cc418933
Nick Seryakov <nick.seryakov@gmail.com>
2020-01-21 23:09:39 +0400

    Add useLayout hook

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,23 @@
+import { useCallback, useState } from 'react';
+import { LayoutChangeEvent } from 'react-native';
+
+type LayoutSize = {
+  width: number;
+  height: number;
+};
+
+export function useLayout(
+  initial?: Partial<LayoutSize>,
+): [LayoutSize, (event: LayoutChangeEvent) => void] {
+  const [size, setSize] = useState(() => ({ width: 0, height: 0, ...initial }));
+  const onLayout = useCallback((event: LayoutChangeEvent) => {
+    const { width, height } = event.nativeEvent.layout;
+    setSize({ width, height });
+  }, []);
+
+  return [size, onLayout];
+}
+
 export function inRange(value: number, min: number, max: number): number {
   return Math.max(min, Math.min(value, max));
 }
