bb5318f632597f6e6d2875807ff74532a8a78ef1
Sergey Alimov <sergey.alimov@gmail.com>
2020-01-24 18:04:54 +0400

    Add Action component + tests

---
+++ src/form/Action.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  StyleProp,
+  Image,
+  ImageSourcePropType,
+  ImageStyle,
+  StyleSheet,
+  Text,
+  TextProps,
+  View,
+} from 'react-native';
+
+import { Theme, Themed, withTheme } from '../theming';
+
+interface ActionProps extends TextProps {
+  children: React.ReactNode;
+  iconSource?: ImageSourcePropType;
+  iconStyle?: StyleProp<ImageStyle>;
+}
+
+const Action: React.FC<Themed<typeof createStyleSheet, ActionProps>> = ({
+  theme: { styles },
+  children,
+  style,
+  iconSource,
+  iconStyle,
+  ...props
+}) => (
+  <View style={styles.root}>
+    <Text {...props} style={[styles.text, style]} numberOfLines={1}>
+      {children}
+    </Text>
+    {!!iconSource && <Image style={[styles.icon, iconStyle]} source={iconSource} />}
+  </View>
+);
+
+const createStyleSheet = ({ colors, fonts }: Theme) =>
+  StyleSheet.create({
+    root: {
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    text: {
+      ...fonts.semibold,
+      height: 16,
+      fontSize: 16,
+      lineHeight: 16,
+      color: colors.indigo,
+    },
+    icon: {
+      width: 22,
+      height: 22,
+      marginRight: -4,
+      tintColor: colors.indigo,
+    },
+  });
+
+export default withTheme(Action, createStyleSheet, 'Form.Action');
