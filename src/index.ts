71cbae5f4f95b8c209a2d1d3d8fa6ef448948162
Nick Seryakov <nick.seryakov@gmail.com>
2020-01-24 04:33:33 +0400

    Create Pager component

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as Avatar } from './avatar/Avatar';
 export { default as Badge } from './badge/Badge';
 export { default as Button } from './buttons/Button';
 export { default as OutlineButton } from './buttons/OutlineButton';
+export { default as Pager } from './pager/Pager';
 export { default as ProgressBar } from './progress/ProgressBar';
 export { default as Rating } from './rating/Rating';
 export { default as Segmented } from './segmented/Segmented';
---
+++ src/pager/Pager.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleProp, StyleSheet, View, ViewProps, ViewStyle } from 'react-native';
+
+import { Themed, WithThemeProps, withTheme } from '../theming';
+
+interface PagerProps extends ViewProps {
+  count?: number;
+  selected?: number;
+  color?: string;
+  orientation?: 'horizontal' | 'vertical';
+  style?: StyleProp<ViewStyle>;
+  itemStyle?: StyleProp<ViewStyle>;
+  selectedItemStyle?: StyleProp<ViewStyle>;
+}
+
+const Pager: React.FC<Themed<typeof createStyleSheet, PagerProps>> = ({
+  theme: { colors, styles },
+  count = 0,
+  selected,
+  color = colors.black,
+  orientation = 'horizontal',
+  style,
+  itemStyle,
+  selectedItemStyle,
+  ...props
+}) => {
+  if (count < 2) {
+    return null;
+  }
+
+  const rootStyles = [styles.root, style];
+
+  if (orientation === 'horizontal') {
+    rootStyles.push({ flexDirection: 'row' });
+  }
+
+  const renderItem = (_item: any, index: number) => {
+    const itemStyles = [{ backgroundColor: color }, styles.item, itemStyle];
+
+    if (index === selected) {
+      itemStyles.push(styles.itemSelected, selectedItemStyle);
+    }
+
+    return <View key={index} style={itemStyles} />;
+  };
+
+  return (
+    <View {...props} style={rootStyles}>
+      {Array.from({ length: count }, renderItem)}
+    </View>
+  );
+};
+
+const createStyleSheet = () =>
+  StyleSheet.create({
+    root: {
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    item: {
+      width: 6,
+      height: 6,
+      margin: 2.5,
+      opacity: 0.5,
+      borderRadius: 3,
+    },
+    itemSelected: {
+      width: 7,
+      height: 7,
+      margin: 2,
+      opacity: 1,
+      borderRadius: 3.5,
+    },
+  });
+
+export type Props = PagerProps & WithThemeProps;
+
+export default withTheme(Pager, createStyleSheet, 'Pager');
