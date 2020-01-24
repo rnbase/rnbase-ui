b0587b3ee97c73a62172bb7f6f2db932970bec3b
Sergey Alimov <sergey.alimov@gmail.com>
2020-01-24 18:09:26 +0400

    Add Row component + tests

---
+++ src/form/Row.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, TouchableHighlight, View, ViewProps } from 'react-native';
+
+import { Themed, withTheme } from '../theming';
+
+interface RowProps extends ViewProps {
+  children: React.ReactNode;
+  onPress?: () => void;
+}
+
+const Row: React.FC<Themed<typeof createStyleSheet, RowProps>> = ({
+  theme: { styles, colors },
+  children,
+  style,
+  onPress,
+  ...props
+}) =>
+  onPress ? (
+    <TouchableHighlight onPress={onPress} activeOpacity={1} underlayColor={colors.underlay}>
+      <View {...props} style={[styles.root, style]}>
+        {children}
+      </View>
+    </TouchableHighlight>
+  ) : (
+    <View {...props} style={[styles.root, style]}>
+      {children}
+    </View>
+  );
+
+const createStyleSheet = () =>
+  StyleSheet.create({
+    root: {
+      minHeight: 48,
+      flexWrap: 'nowrap',
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 15,
+      justifyContent: 'space-between',
+    },
+  });
+
+export default withTheme(Row, createStyleSheet, 'Form.Row');
