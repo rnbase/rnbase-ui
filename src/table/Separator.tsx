7dbad37018d702d9168e8dd3d23cb10b429ac76c
Nick Seryakov <nick.seryakov@gmail.com>
2020-01-29 23:58:37 +0400

    Adjust Separator component

--- src/table/Separator.tsx
+++ src/table/Separator.tsx
@@ -4,25 +4,31 @@ import { StyleSheet, View, ViewProps } from 'react-native';
 import { Theme, Themed, withTheme } from '../theming';
 
 interface SeparatorProps extends ViewProps {
+  insetLeft?: number;
   highlighted?: boolean;
 }
 
 const Separator: React.FC<Themed<typeof createStyleSheet, SeparatorProps>> = ({
   theme: { styles },
-  style,
+  insetLeft = 20,
   highlighted,
+  style,
   ...props
-}) => <View {...props} style={[styles.root, style, highlighted && styles.highlighted]} />;
+}) => (
+  <View
+    {...props}
+    style={[{ marginLeft: insetLeft }, styles.root, style, highlighted && styles.highlighted]}
+  />
+);
 
 const createStyleSheet = ({ colors }: Theme) =>
   StyleSheet.create({
     root: {
-      marginLeft: 20,
       height: StyleSheet.hairlineWidth,
       backgroundColor: colors.separator,
     },
     highlighted: {
-      backgroundColor: 'transparent',
+      opacity: 0,
     },
   });
 
